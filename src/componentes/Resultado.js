05944db008ad8c6dba1907cb476cff7facd76fc8
Pol Reig <polreigbroto@gmail.com>
2021-06-18 19:13:28 +0200

    Eliminar palabra onClick

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -1,4 +1,22 @@
+import PropTypes from "prop-types";
+
 export const Resultado = (props) => {
-  const { palabra } = props;
-  return <li>{palabra.palabra}</li>;
+  const { palabra, setDisplayPalabras, displayPalabras } = props;
+  const clearPalabara = () =>
+    setDisplayPalabras(
+      displayPalabras.filter((element) => element.id !== palabra.id)
+    );
+  return <li onClick={clearPalabara}>{palabra.palabra}</li>;
+};
+
+Resultado.propTypes = {
+  palabra: PropTypes.shape({
+    palabra: PropTypes.string.isRequired,
+    id: PropTypes.number.isRequired,
+    numeroCaracteres: PropTypes.number.isRequired,
+    lenguajeProgramacion: PropTypes.bool.isRequired,
+    maximoVeces: PropTypes.string.isRequired,
+  }).isRequired,
+  displayPalabras: PropTypes.array.isRequired,
+  setDisplayPalabras: PropTypes.func.isRequired,
 };
