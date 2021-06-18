f86e409fb8f5c1759237aaf434d2b4207e6b436a
Raúl Navarro <raul.navarro.uribe@gmail.com>
2021-06-18 23:17:08 +0200

    Primera letra mayúscula solo si es primer objeto del array

--- src/componentes/Resultado.js
+++ src/componentes/Resultado.js
@@ -2,14 +2,14 @@ import PropTypes from "prop-types";
 
 export const Resultado = (props) => {
   const { palabra, setCopiaListadoPalabras, copiaListadoPalabras } = props;
-  const palabraMayusculua = palabra.id === 1;
+
   const clearPalabara = () =>
     setCopiaListadoPalabras(
       copiaListadoPalabras.filter((element) => element.id !== palabra.id)
     );
   return (
     <li onClick={clearPalabara}>
-      {palabraMayusculua
+      {copiaListadoPalabras.indexOf(palabra) === 0
         ? palabra.palabra[0].toUpperCase() + palabra.palabra.slice(1)
         : palabra.palabra}
     </li>
