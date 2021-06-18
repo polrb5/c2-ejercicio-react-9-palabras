a75d8658d15618336f595d53eb7e5380c8444fb7
Pol Reig <polreigbroto@gmail.com>
2021-06-18 16:06:51 +0200

    Componentes credos y fuente importada

---
+++ src/componentes/ListaPalabras.js
@@ -0,0 +1,30 @@
+export const ListaPalabras = () => {
+  return (
+    <ul className="lista-palabras">
+      <li>me</li>
+      <li data-lenguaje="si">JavaScript</li>
+      <li>el</li>
+      <li>la</li>
+      <li>gusta</li>
+      <li>odio</li>
+      <li>no</li>
+      <li>muy</li>
+      <li>y</li>
+      <li data-lenguaje="si">Java</li>
+      <li>mucho</li>
+      <li>de</li>
+      <li>poco</li>
+      <li>programador</li>
+      <li>amigo</li>
+      <li>programadora</li>
+      <li>amiga</li>
+      <li>soy</li>
+      <li>también</li>
+      <li>pero</li>
+      <li>gustan</li>
+      <li>gatos</li>
+      <li>los</li>
+      <li>perros</li>
+    </ul>
+  );
+};
---
+++ src/componentes/Palabras.js
@@ -0,0 +1,11 @@
+import { ListaPalabras } from "./ListaPalabras";
+import { Resultado } from "./Resultado";
+
+export const Palabras = () => {
+  return (
+    <section className="palabras">
+      <ListaPalabras />
+      <Resultado />
+    </section>
+  );
+};
